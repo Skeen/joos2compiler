a0e4d23701b873b6926b3d2fe2143d292aa06d1c
Emil 'Skeen' Madsen <sovende@gmail.com>
2013-09-04 13:56:42 +0200

    Updated ErrorHandling, such that it now reports only the string, that caused the error, and point to the specific point within that string, where it happend.

--- src/Error.cpp
+++ src/Error.cpp
@@ -11,39 +11,69 @@ namespace Error
     {
     }
 
-    Generic_Error::Generic_Error(std::string error_text_init, std::string::iterator, std::string::iterator end, std::string::iterator issue)
+    Generic_Error::Generic_Error(std::string error_text_init, std::string::iterator begin, std::string::iterator end, std::string::iterator issue)
     {
-        error_text  = error_text_init;
+        // Get the error text string
+        error_text = error_text_init;
+        error_text.append(":\n");
 
-        error_text.append(std::string(issue, end));
-/*
-        std::string::iterator line_start = raw_input_start;
-        std::string::iterator line_end = raw_input_start;
-
-        std::string raw_input(raw_input_start, raw_input_end);
-        std::istringstream raw_input_stream(raw_input);
-
-        while(raw_input_stream)
+        // Create a string, from the beginning of our input, to the issue
+        std::string start_input(begin, issue);
+        // Now let's detect the last line break, before the line, where begin is
+        std::string::iterator line_start;
+        // Search for the last newline
+        size_t found = start_input.find_last_of("\n");
+        // If we didnt find one;
+        if(found == std::string::npos)
         {
-            if(line_start < begin && begin < line_end)
-            {
-                std::cout << "FOUND";
-                break;
-            }
-            std::string line;
-            getline(raw_input_stream, line);
-
-            line_start = line.begin();
-            line_end = line.end();
+            // Use the beginning of the file
+            line_start = begin;
         }
+        // If we found one
+        else
+        {
+            // Let's set line start to the character after that
+            line_start = begin + found + 1;
+        }
+        // At this point line_start points to the first character of the faulty line
+        // We'll save this for later!
 
-        std::string line(line_start, line_end);
-        error_text.append(to_string(line.length()));
-        //error_text.append(to_string(line_number));
-        error_text.append("[");
-        error_text.append(line);
-        error_text.append("]");
-        */
+        // Create a string, form the issue, to the end of our input
+        std::string end_input(issue, end);
+        // Now let's detect the first line break, after the line, where begin is
+        std::string::iterator line_end;
+        // Search for the first newline
+        found = end_input.find_first_of("\n");
+        // If we didnt find one;
+        if(found == std::string::npos)
+        {
+            // Use the end of the file
+            line_end = end;
+        }
+        // If we found one
+        else
+        {
+            // Let's set line end to the character before that
+            line_end = issue + found;
+        }
+        // At this point line_end points to the last character of the faulty line
+        // Let's create a string, holding the exact faulty line
+        std::string faulty_line(line_start, line_end);
+        // Append the faulty line to the error_text
+        error_text.append(faulty_line).append("\n");
+        // Now let's find out where to put our fine small '^^^' to indicate the error
+        // Let's find the number of characters from the start of the line, until
+        // the issue;
+        size_t num_required_spaces = std::distance(line_start, issue);
+        // Then let's find the length of the rest of the error.
+        size_t num_following_wedges = faulty_line.size() - num_required_spaces;
+        // Now let's generate our string. (using fill constructors)
+        std::string error_indicator_spacing(num_required_spaces, ' ');
+        std::string error_indicator_indicator(num_following_wedges, '^');
+        // And append this indicator to the error_text
+        error_text.append(error_indicator_spacing).append(error_indicator_indicator).append("\n");
+
+
     }
     /*
        Generic_Error(std::string error_text_init, std::string raw_input, std::string::iterator begin, std::string::iterator end)
@@ -61,12 +91,12 @@ namespace Error
     }
 
     Syntax_Error::Syntax_Error()
-        : Generic_Error(std::string("Syntax Error:\n"))
+        : Generic_Error(std::string("Syntax Error"))
     {
     }            
 
     Syntax_Error::Syntax_Error(std::string::iterator raw_input_start, std::string::iterator raw_input_end, std::string::iterator begin)
-        : Generic_Error(std::string("Syntax Error:\n"), raw_input_start, raw_input_end, begin)
+        : Generic_Error(std::string("Syntax Error"), raw_input_start, raw_input_end, begin)
     {
     }
 }
